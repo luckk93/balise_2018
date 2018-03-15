270a9849f96666fd1702b04ecaa0d56cf81b0db0
luckk93 <luckk93@gmail.com>
2018-03-15 16:50:29 +0100

    caribr

--- patternSearch.cpp
+++ patternSearch.cpp
@@ -260,7 +260,7 @@ bool patternSearch(Mat analyseImg, int (&pattern)[3])
         inRange(PathZoneBlackMask, Scalar(90,60,50),Scalar(125,256,256), squareSearch[0]); //blue
         inRange(PathZoneBlackMask, Scalar(0,120,50),Scalar(10,256,256), squareSearch[1]);    //orange
         inRange(PathZoneBlackMask, Scalar(40,40,50),Scalar(89,256,256), squareSearch[2]);   //green
-        inRange(PathZoneBlackMask, Scalar(15,120,50),Scalar(35,256,256), squareSearch[3]);   //yellow
+        inRange(PathZoneBlackMask, Scalar(15,120,100),Scalar(35,256,256), squareSearch[3]);   //yellow
         inRange(PathZoneWhiteMask, Scalar(0,0,0),Scalar(190,256,70), squareSearch[4]);       //black
 
         
