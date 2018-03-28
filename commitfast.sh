a22f184cfd5e488b733d51be25eb6afb59ef415c
luckk93 <luckk93@gmail.com>
2018-03-28 20:55:21 +0000

    fastcommit

--- commitfast.sh
+++ commitfast.sh
@@ -2,13 +2,8 @@
 sudo  git config --global user.email "luckk93@gmail.com"
 sudo git config --global user.name "luckk93"
 
-<<<<<<< HEAD
 sudo git add .
 sudo git commit -m "fastcommit"
-=======
->>>>>>> bdf1293d7390d9baf5d90d90ffee67be2e1adea3
 sudo git pull origin master
-sudo git add .
-sudo git commit -m "fastcommit"
 sudo git push origin master
 
